14e845911fe8fe93a8ffb10543f207aab9c7a355
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-04-07 16:26:25 +0300

    "lint:js": "eslint src/**/*.{js,jsx,ts}"

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { ITodoItem } from 'interfaces/TodoItem.interface';
 
 export const TodoItem: React.FC<ITodoItem> = props => {
-  const { id, title } = props;
+  const { title } = props;
 
   return (
     <div>
-      <span>{id}</span>
       <span>{title}</span>
     </div>
   );
