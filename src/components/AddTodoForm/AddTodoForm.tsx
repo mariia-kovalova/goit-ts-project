1c4c56b0aa0b2f3a49e31726568902e8085ec216
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-04-09 18:16:12 +0300

    react hook form

--- src/components/AddTodoForm/AddTodoForm.tsx
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -1,59 +1,66 @@
+import { FC } from 'react';
 import { nanoid } from '@reduxjs/toolkit';
 import { useAppDispatch } from 'hooks/useRedux';
-import { FC, ChangeEvent, FormEvent, useState } from 'react';
 import { addTodo } from 'redux/todos/operations';
-import { OnlyTitle } from 'types/TodoItem.interface';
-
+import { SubmitHandler, useForm } from 'react-hook-form';
 import {
   Container,
   FormControl,
   FormLabel,
   Input,
   Button,
-  FormHelperText,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 
-const initislState = { title: '' };
+type Inputs = {
+  title: string;
+};
 
 const taskId = nanoid();
+const titleId = nanoid();
 
 export const AddTodoForm: FC = () => {
-  const [todo, setTodo] = useState<Partial<OnlyTitle>>(initislState);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({ defaultValues: { title: '' } });
+
   const dispatch = useAppDispatch();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTodo({ title: e.target.value });
-  };
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    if (!todo.title || todo.title === '') {
-      return alert('This field is required.');
-    }
+  const onSubmit: SubmitHandler<Inputs> = values => {
     dispatch(
       addTodo({
         id: taskId,
-        title: todo.title,
         isCompleted: false,
+        ...values,
       })
     );
-    setTodo(initislState);
   };
 
   return (
     <Container>
-      <FormControl onSubmit={handleSubmit}>
-        <FormLabel htmlFor="title">Title</FormLabel>
-        <Input
-          type="text"
-          name="title"
-          id="title"
-          onChange={handleChange}
-          value={todo.title}
-        />
-        <FormHelperText>Enter your todo</FormHelperText>
-        <Button type="submit">Add todo</Button>
-      </FormControl>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl
+          isInvalid={errors.title && true}
+          display="flex"
+          alignItems="center"
+        >
+          <FormLabel htmlFor={titleId}>Title</FormLabel>
+          <Input
+            type="text"
+            id={titleId}
+            {...register('title', {
+              required: 'This is required',
+              minLength: { value: 2, message: 'Minimum length should be 4' },
+            })}
+          />
+          <FormErrorMessage>Error</FormErrorMessage>
+        </FormControl>
+        <Button type="submit" disabled={isSubmitting}>
+          Add todo
+        </Button>
+      </form>
     </Container>
   );
 };
